22826fe13ed78fcff890dbb64232548f085530c6
a.plaksin@demis.ru <a.plaksin@demis.ru>
2016-08-31 16:31:39 +0300

    thread safe logging system prototype

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
-#include <iostream>
-
 #include "gladius/gladius.h"
-
-#undef main
 
 #ifdef PLATFORM_WINDOWS
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 #endif
 
 #ifdef PLATFORM_LINUX
+#undef main
 int main (int argc, char *argv[])
 #endif
 {
